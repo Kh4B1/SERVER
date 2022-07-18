8bbb41c694fe40b9545b55a7b8501249fa59d553
pino._.hoo <inhoo23@naver.com>
2022-07-18 11:22:17 +0900

    김인후 Add JWT

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,16 +1,15 @@
-const pool = require("../../config/database"),
-  bcrypt = require("bcryptjs"),
-  nodemailer = require("nodemailer"),
-  emailData = require("../../config/emailData"),
-  jwt = require("jsonwebtoken"),
-  secretKey = "Secret_Key"
+const pool = require('../../config/database'),
+  bcrypt = require('bcryptjs'),
+  nodemailer = require('nodemailer'),
+  emailData = require('../../config/emailData'),
+  jwt = require('jsonwebtoken'),
+  secretKey = 'Secret_Key'
 
 exports.login = (req, res) => {
-  console.log(req.body)
   const param = [req.body.email, req.body.pw],
-    accessToken = jwt.sign({ id: param[0] }, secretKey, { expiresIn: "1h" })
+    accessToken = jwt.sign({ id: param[0] }, secretKey, { expiresIn: '1h' })
   pool((conn) => {
-    const sql = "select * from tbl_user where id = ?"
+    const sql = 'select * from tbl_user where email = ?'
     conn.query(sql, param[0], (err, row) => {
       if (err) res.send({ result: false, message: err })
       if (row.length > 0) {
@@ -18,9 +17,9 @@ exports.login = (req, res) => {
           err && res.send({ result: false, message: err })
           if (result) {
             res.send({ result: true, token: accessToken })
-          } else res.send({ result: false, message: "PW ERR" })
+          } else res.send({ result: false, message: 'PW ERR' })
         })
-      } else res.send({ result: false, message: "ERROR" })
+      } else res.send({ result: false, message: 'ERROR' })
     })
     conn.release()
   })
@@ -28,10 +27,11 @@ exports.login = (req, res) => {
 
 exports.register = (req, res) => {
   const { email, pw, name } = req.body
+  console.log(email)
   bcrypt.hash(pw, 10, (err, hash) => {
     if (err) res.send({ result: false, message: err })
     pool((conn) => {
-      const sql = "insert into tbl_user(email, name, pw) values(?,?,?)"
+      const sql = 'insert into tbl_user(email, name, pw) values(?,?,?)'
       conn.query(sql, [email, name, hash], (err, result) => {
         err
           ? res.send({ result: false, message: err })
@@ -45,9 +45,9 @@ exports.register = (req, res) => {
 exports.checkEmail = async (req, res) => {
   const checkCode = String(emailData.number())
   const transporter = nodemailer.createTransport({
-    service: "Naver",
+    service: 'Naver',
     prot: 587,
-    host: "smtp.naver.com",
+    host: 'smtp.naver.com',
     secure: false,
     requireTLS: true,
     auth: {
@@ -59,7 +59,7 @@ exports.checkEmail = async (req, res) => {
   const mailOptions = {
     from: emailData.user,
     to: req.body.email,
-    subject: "[ANYAD Sign Up Check Code]",
+    subject: '[ANYAD Sign Up Check Code]',
     text: `Your Code : ${checkCode}`,
   }
   transporter.sendMail(mailOptions, (err, info) => {
---
+++ middlewares/jwt.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken'),
+  secretKey = 'Secret_Key'
+
+exports.check = (req, res, next) => {
+  const token = req.headers.accesstoken
+  console.log(token)
+
+  const p = new Promise((resolve, reject) => {
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) reject(err)
+      resolve(decoded)
+    })
+  })
+
+  p.then((decoded) => {
+    req.body.token = token
+    req.body.user = decoded
+    next()
+  }).catch((err) => {
+    res.send({
+      success: false,
+      message: err.message,
+    })
+  })
+}
