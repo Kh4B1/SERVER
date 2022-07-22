181d54bea9196431c80d214d0fd82a8f33a60f0b
pino._.hoo <inhoo23@naver.com>
2022-07-22 15:58:51 +0900

    Update sequelize

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
   cors = require('cors'),
-  api = require('./api')
+  api = require('./api'),
+  { sequelize } = require('./models'),
+  models = require('./models')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공')
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
 app.use('/api', api)
-app.get('/' , (req, res) => res.send(`SERVER ON! PORT  : ${port}`))
+app.get('/', (req, res) => res.send(`SERVER ON! PORT  : ${port}`))
+app.get('/test', async (req, res) => {
+  const data = await models.Board.findAll()
+  res.send({ result: true, data: data })
+})
 const port = 8000
 app.listen(port, () => console.log(`SERVER ON! PORT : ${port}`))
---
+++ models/Access.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Access = sequelize.define(
+    'Access',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: 'PK',
+      },
+    },
+    {
+      charset: 'utf8', // 한국어 설정
+      collate: 'utf8_general_ci', // 한국어 설정
+      tableName: 'tbl_access', // 테이블 이름
+      timestamps: true, // createAt & updateAt 활성화
+      paranoid: true, // timestamps 가 활성화 되어야 사용 가능 > deleteAt 옵션 on
+    },
+  )
+  Access.associate = (models) => {
+    Access.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      sourceKey: 'id',
+    })
+    Access.belongsTo(models.Module, {
+      foreignKey: 'module_id',
+      sourceKey: 'id',
+    })
+    Access.hasMany(models.Video, { foreignKey: 'access_id', sourceKey: 'id' })
+  }
+  return Access
+}
